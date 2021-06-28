242d292e16abf66d4ab772ca6262d34ac806f8dd
Naman Tamrakar <59503233+namantam1@users.noreply.github.com>
2021-06-28 12:09:27 +0530

    Update settings.py

--- tempfiler/settings.py
+++ tempfiler/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'tempfiler.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'test': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
